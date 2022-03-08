d05c1143ea16ff5100eacbea48791ad9499ed713
barreirobenjamin <barreirobenjamin@gmail.com>
2022-03-08 20:00:40 -0300

    ejercitacion clase 4

---
+++ clase_4.js
@@ -0,0 +1,35 @@
+/* let temperatura = 20;
+let estaLloviendo = false;
+let pisoAsensor = 7;
+let horaActual = 12;
+let horaApertura = 10;
+let horaCierre = 22; */
+
+/* let cantidadQueso = 120;
+let deudaMonica = -500; */
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+/* segunda parte A*/
+/* let cantidadLeche = prompt("Leche: ");
+console.log("Cantidad: ", cantidadLeche);
+
+let esDeDia = prompt("Dia: ");
+console.log("Es de dia: ", esDeDia);
+let tengoDinero = prompt("Tengo dinero: ");
+console.log("Dinero: ", tengoDinero);
+
+let deudaJuan = prompt("Deuda: ");
+console.log("Deuda: ", deudaJuan); */
+
+/* segunda parte A*/
+let horaApertura = 8;
+let horaCierre = 21;
+
+
+let horaActual = prompt("Hora: ");
+console.log("Hora: ", horaActual);
+
+let estaAbierto = prompt("esta abierto: ");
+console.log("esta abierto: ", estaAbierto);
+
